92bfa73b7c288ac824dc652d02e52c07236cd3c7
Shadrach <shadrachp@yahoo.com>
2024-06-05 13:41:02 +0800

    Add popover component
    > Add popover component that anchors below
    an action component.

---
+++ src/components/Popover/index.jsx
@@ -0,0 +1,66 @@
+import { useRef, useState } from 'react';
+
+import './styles.css';
+
+const Popover = ({ action, btnClass, label, children }) => {
+    const [currentState, setCurrentState] = useState('hide');
+    const [canToggle, setCanToggle] = useState(true);
+    const popoverRef = useRef(null);
+
+    const togglePopover = () => {
+        const shouldShowPopover = currentState === 'hide';
+        if (canToggle) {
+            setCurrentState(shouldShowPopover ? 'show' : 'hide');
+            setCanToggle(false);
+            if (shouldShowPopover) {
+                console.log("test", {shouldShowPopover});
+                document.addEventListener('mousedown', handleOutsideClick, false);
+            } else {
+                document.removeEventListener('mousedown', handleOutsideClick, false);
+            }
+            if (action) {
+                action(shouldShowPopover);
+            }
+            // setTimeout(() => {
+            //     setCanToggle(true);
+            // }, 250);
+            setCanToggle(true);
+        }
+    };
+
+    const handleClick = () => {
+        togglePopover();
+    };
+
+    const handleOutsideClick = (e) => {
+        // TODO: Fix bug with outside click
+        // if (popoverRef.current && !popoverRef.current.contains(e.target)) {
+        //     togglePopover();
+        // }
+    };
+
+    return (
+        <div className="container">
+            <a
+                href="#"
+                className={btnClass}
+                onClick={(e) => {
+                    e.preventDefault();
+                    handleClick();
+                }}
+            >
+                {label}
+            </a>
+            <div
+                className={`popover popover--${currentState}`}
+                ref={(node) => {
+                    popoverRef.current = node;
+                }}
+            >
+                {children}
+            </div>
+        </div>
+    );
+}
+
+export default Popover;
